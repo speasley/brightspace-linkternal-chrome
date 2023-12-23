f93790aa376ec144e22f230aa133fdc370fa5e4b
Stephen Peasley <speasley@users.noreply.github.com>
2023-12-23 11:52:02 -0700

    feat: first cut

---
+++ README.md
@@ -0,0 +1,42 @@
+# Brightspace Linkternal Chrome Extension
+
+## Description
+**Brightspace Linkternal** make external links in assessment submissions open in a new tab in the [D2L Brightspace Learning Management System](https://www.d2l.com/brightspace/) UI.
+
+## Installation
+
+### From the Chrome Web Store (Recommended)
+
+1. Visit the [Brightspace Linkternal extension page](https://chromewebstore.google.com/detail/brightspace-linkternal/jjipgkhomehdhimjffjpghakjjkdijag?hl=en).
+
+2. Click the "Add to Chrome" button.
+
+3. Follow the on-screen instructions to complete the installation.
+
+### Manual Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/speasley/brightspace-linkternal-chrome.git
+   ```
+
+2. Open Google Chrome and go to chrome://extensions/.
+
+3. Enable "Developer mode".
+
+4. Click the "Load unpacked" button.
+
+5. Select the folder where you cloned the extension repository and click "Open."
+
+### Licence
+
+This project is licensed under the [MIT Licence](http://opensource.org/licenses/MIT).
+
+### Privacy Policy
+
+While this extension collects no data, you can still read its applicable [privacy policy](./PRIVACY.md).
+
+### Disclaimer
+
+Neither this extension nor its author are affiliated with or endorsed by D2L. This is an independent project created for the purpose of reducing potential evaluation bias.
---
+++ scripts/content.js
@@ -0,0 +1,58 @@
+setTimeout(() => {
+    const isD2l = document.querySelector("body.d2l-body");
+
+    const findElementsInShadowDom = (root, className, findFirst = false) => {
+        let elements = [];
+
+        if (root?.classList?.contains(className)) {
+            if (findFirst) return root;
+            elements.push(root);
+        }
+
+        if (root?.shadowRoot) {
+            const foundInShadow = findElementsInShadowDom(root.shadowRoot, className, findFirst);
+            if (findFirst && foundInShadow) return foundInShadow;
+            elements = elements.concat(foundInShadow);
+        }
+
+        if (root?.children) {
+            for (const child of root.children) {
+                const foundInChildren = findElementsInShadowDom(child, className, findFirst);
+                if (findFirst && foundInChildren) return foundInChildren;
+                elements = elements.concat(foundInChildren);
+            }
+        }
+
+        return findFirst ? null : elements;
+    };
+
+    const injectStylesIntoShadowDom = (element, css) => {
+        if (element.shadowRoot) {
+            const style = document.createElement('style');
+            style.textContent = css;
+            element.shadowRoot.appendChild(style);
+        }
+    };
+
+    if (isD2l) {
+        const rootElement = document.querySelector("body.d2l-body");
+        
+        // Simplified to a single function call
+        const submissionListView = findElementsInShadowDom(rootElement, "d2l-consistent-evaluation-submission-list-view", true);
+
+        if (submissionListView) {
+            const submissionItems = findElementsInShadowDom(submissionListView, "d2l-submission-item-text");
+            submissionItems.forEach(element => {
+                let items = findElementsInShadowDom(element, "d2l-html-block-rendered");
+                items.forEach(item => {
+                    const links = item.querySelectorAll("a");
+                    links.forEach(link => {
+                        link.classList.add("brightspace-linkternal");
+                        link.innerHTML += '<span class="link-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="height: auto; transform: translate(2px, -4px); width: 13px;"><path d="M14 5a.94.94 0 0 1-1-1 .94.94 0 0 1 1-1h6c.3 0 .5.1.7.3s.3.4.3.7v6a.94.94 0 0 1-1 1 .94.94 0 0 1-1-1V6.4l-9.3 9.3c-.4.4-1 .4-1.4 0s-.4-1 0-1.4L17.6 5H14zM3 7c0-1.1.9-2 2-2h5a.94.94 0 0 1 1 1 .94.94 0 0 1-1 1H5v12h12v-5a.94.94 0 0 1 1-1 .94.94 0 0 1 1 1v5c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V7z"/></svg></span>';
+                        link.setAttribute("target", "_blank");
+                    });
+                });
+            });
+        }
+    }
+}, 4000);
